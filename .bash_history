bf7b1a53889b6b279879b3db93fa0036495e036d
kuraoka <kuraoka@jp.ibm.com>
2017-01-31 10:28:43 +0000

    done

---
+++ .bash_history
@@ -0,0 +1,15 @@
+ls
+cd app
+ls
+cat package.json 
+cd node_modules/
+ls
+cd
+find . -iname "dash*"
+find . -iname "dashboard*"
+find . -iname "*dashboard*"
+ls
+grep "npm update" *
+cd app
+grep "npm update" *
+uname -a
---
+++ app/.profile.d/bluemix_env.sh
@@ -0,0 +1,7 @@
+export MANAGEMENT_SERVER_URL="https://rtmgmt.ng.bluemix.net"
+export cloud_controller_url="https://api.ng.bluemix.net"
+export authorization_endpoint="https://login.ng.bluemix.net/UAALoginServerWAR"
+export BOOT_SCRIPT=node_modules/node-red/red.js
+export NODE_EXECUTABLE=node
+export NODE_OPTS="--max-old-space-size=384"
+export NODE_ARGS="--settings ./bluemix-settings.js -v"
